4cd2ffa1951058cb6e0b4ba737dcb3ee4bd68d30
hqs IT master <86848510+HQS2KEQIN@users.noreply.github.com>
2023-05-22 08:38:56 +0800

    Delete stu_system/src directory

--- stu_system/src/input_email.c
+++
@@ -1,8 +0,0 @@
-#include<string.h>
-#include<stdio.h>
-void input_email(char *str)
-{
-	memset(str, 0, sizeof(str));
-	printf("«Î ‰»Î” œ‰:");
-	scanf("%s",str);
-}
--- stu_system/src/input_password.c
+++
@@ -1,8 +0,0 @@
-#include<string.h>
-#include<stdio.h>
-void input_password(char *str)
-{
-	memset(str, 0, sizeof(str));
-	printf("ÇëÊäÈëÃÜÂë:");
-	scanf("%s",str);
-}
